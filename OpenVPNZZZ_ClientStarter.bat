5b1fa9c2b472135a4809c3e8218e92edf86cfd53
firak01 <firak01@t-online.de>
2020-07-03 09:57:05 +0200

    Starte als Server über JAR
    Signed-off-by: firak01 <firak01@t-online.de>

--- OpenVPNZZZ_ClientStarter.bat
+++ OpenVPNZZZ_ClientStarter.bat
@@ -1,5 +1,5 @@
 REM ALS AUSFÜHRBARE DATE MIT EINEM FESTGELEGTEN STARTPUNKT: java -jar OpenVPNZZZ_V20200613.jar -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_StarterLog.txt
 REM Beliebige Klasse mit einer Methode: public static void main(String[] args) {} starten. 
 REM Merke: Wenn die JAR - Datei nicht in dem Verzeichnis der Batch liegt, den Pfad vorneweg ergänzen.
-java -cp OpenVPNZZZ_V20200701.jar use.openvpn.clientui.ClientMainUIZZZ -d . -f ZKernelConfig_OVPNClient.ini > c:\fglkernel\kernellog\OpenVPNZZZ_ClientStarterLog.txt
+java -cp OpenVPNZZZ_V20200703.jar use.openvpn.clientui.ClientMainUIZZZ -d . -f ZKernelConfig_OVPNClient.ini > c:\fglkernel\kernellog\OpenVPNZZZ_ClientStarterLog.txt
 pause
--- OpenVPNZZZ_ServerStarter.bat
+++ OpenVPNZZZ_ServerStarter.bat
@@ -1,1 +1,5 @@
-java -jar OpenVPNZZZ_V20200613.jar -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_StarterLog.txt
+REM ALS AUSFÜHRBARE DATE MIT EINEM FESTGELEGTEN STARTPUNKT: java -jar OpenVPNZZZ_V20200613.jar -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_StarterLog.txt
+REM Beliebige Klasse mit einer Methode: public static void main(String[] args) {} starten. 
+REM Merke: Wenn die JAR - Datei nicht in dem Verzeichnis der Batch liegt, den Pfad vorneweg ergänzen.
+java -cp OpenVPNZZZ_V20200703.jar use.openvpn.serverui.ServerMainUIZZZ -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_ServerStarterLog.txt
+pause
