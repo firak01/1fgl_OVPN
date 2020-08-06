e3c9eebe6dc10859fe14d3ec4f74d34ce495af27
firak01 <firak01@t-online.de>
2020-08-06 08:31:03 +0200

    Weitere Testklasse - zum Starten per jar
    Signed-off-by: firak01 <firak01@t-online.de>

--- KernelZZZTest_GUIStarter_JarEasyInCurrentJar.bat
+++ KernelZZZTest_GUIStarter_JarEasyInCurrentJar.bat
@@ -1,5 +1,6 @@
 ﻿REM ALS AUSFÜHRBARE DATE MIT EINEM FESTGELEGTEN STARTPUNKT: java -jar OpenVPNZZZ_V20200613.jar -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_StarterLog.txt
 REM Beliebige Klasse mit einer Methode: public static void main(String[] args) {} starten. 
 REM Merke: Wenn die JAR - Datei nicht in dem Verzeichnis der Batch liegt, den Pfad vorneweg ergänzen.
-java -cp OpenVPNZZZ_V20200801.jar use.openvpn.clientui.ClientMainUIZZZ -s 02 -d . -f ZKernelConfig_OVPNClient.ini > c:\fglkernel\kernellog\OpenVPNZZZ_ClientStarterLog.txt
+REM TEST IST HIER: JAZKernel\test\basic\zBasic\util\file\JarEasyZZZTestMain.java
+java -cp OpenVPNZZZ_V20200801.jar basic.zBasic.util.file.JarEasyInCurrentJarZZZTestMain > c:\fglkernel\kernellog\JarEasyInCurrentJarZZZTest_StarterLog.txt
 pause
