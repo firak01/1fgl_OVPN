e8fa9771e92ca073932c9acabde75f0bcfca31e3
firak01 <firak01@t-online.de>
2020-11-25 08:51:26 +0100

    Dev04VM
    Signed-off-by: firak01 <firak01@t-online.de>

--- OpenVPNZZZ_ClientStarter.bat
+++ OpenVPNZZZ_ClientStarter.bat
@@ -1,5 +1,7 @@
 ﻿REM ALS AUSFÜHRBARE DATE MIT EINEM FESTGELEGTEN STARTPUNKT: java -jar OpenVPNZZZ_V20200613.jar -d . -f ZKernelConfig_OVPNServer.ini > c:\fglkernel\kernellog\OpenVPNZZZ_StarterLog.txt
 REM Beliebige Klasse mit einer Methode: public static void main(String[] args) {} starten. 
 REM Merke: Wenn die JAR - Datei nicht in dem Verzeichnis der Batch liegt, den Pfad vorneweg ergänzen.
+cd\
+cd C:\1fgl\client\JAZKernel
 java -cp OpenVPNZZZ_V20201005.jar use.openvpn.clientui.ClientMainUIZZZ -s 02 -d . -f ZKernelConfig_OVPNClient.ini > c:\fglkernel\kernellog\OpenVPNZZZ_ClientStarterLog.txt
 pause
